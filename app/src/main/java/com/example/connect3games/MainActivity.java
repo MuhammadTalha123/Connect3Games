035304c1b0d18cf78723d7968af388b80d6c8cc7
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-05-19 00:27:32 +0500

    update text

--- app/src/main/java/com/example/connect3games/MainActivity.java
+++ app/src/main/java/com/example/connect3games/MainActivity.java
@@ -3,8 +3,82 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+
+     // (0) for yellow r (1) for red :2 for empty
+
+     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+     boolean gameActive = true;
+
+     int activePlayer = 0;
+
+    public void dropIn (View view) {
+
+        ImageView counter = (ImageView) view;
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2 && gameActive) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            for (int[] winningPosition : winningPositions) {
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+
+                    // someone has won
+                    gameActive = false ;
+
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+
+                        winner = "yellow";
+
+                    } else {
+
+                        winner = "red";
+
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+                    winnerTextView.setText(winner + " has won!");
+
+                    playAgainButton.setVisibility(View.VISIBLE);
+
+                    winnerTextView.setVisibility(View.VISIBLE );
+
+                }
+
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
